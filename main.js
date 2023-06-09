371c763dfbc3be6b204ffcf2c002fd9ea0af1acc
Jeronimo <jeronimocomaschi05@gmail.com>
2023-06-09 18:53:44 -0300

    retocando estilos de la version 1

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const $quincePorcinto = document.querySelector('#quince')
 const $veintePorcinto = document.querySelector('#veinte')
 const $veinticincoPorcinto = document.querySelector('#veinticinco')
 const $propina = document.querySelector('#propina')
+const $div = document.querySelector('#result')
 
 
 function calcularPorcentaje(porcentaje) {
     const value = Number($input.value)
     const propina = value * porcentaje
+    $div.classList.remove('inactive')
 
-    $propina.textContent = Math.trunc(propina)
+    $propina.textContent = `$${Math.trunc(propina)}`
 }
