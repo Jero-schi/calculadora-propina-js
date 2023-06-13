5b466f1a2b6c53851f7b040bf1b41407097d3f95
Jeronimo <jeronimocomaschi05@gmail.com>
2023-06-13 16:06:38 -0300

    cambiando los botones por un rango, sin responsive

--- main.js
+++ main.js
@@ -5,12 +5,24 @@ const $veintePorcinto = document.querySelector('#veinte')
 const $veinticincoPorcinto = document.querySelector('#veinticinco')
 const $propina = document.querySelector('#propina')
 const $div = document.querySelector('#result')
+const $rango = document.querySelector('#rango')
+const $parrafo = document.querySelector('#parrafo')
+const $rectMoney = document.querySelector('#rect-money')
+let valueRango = $rango.valueAsNumber
 
+// function calcularPorcentaje(porcentaje) {
+//     const value = Number($input.value)
+//     const propina = value * porcentaje
+//     $div.classList.remove('inactive')
 
-function calcularPorcentaje(porcentaje) {
+//     $propina.textContent = `$${Math.trunc(propina)}`
+// }
+
+$rango.addEventListener('input', () => {
+    valueRango = $rango.valueAsNumber
+    $parrafo.textContent = `${valueRango} %`;
     const value = Number($input.value)
-    const propina = value * porcentaje
-    $div.classList.remove('inactive')
-
+    const propina = value * valueRango / 100
+    
     $propina.textContent = `$${Math.trunc(propina)}`
-}
+})
